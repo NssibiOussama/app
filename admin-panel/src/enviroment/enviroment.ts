dfb5132710e3b9edb2c15a501edc488c6c6995db
NssibiOussama <oussama.ncibi@tek-up.de>
2025-02-09 21:25:37 +0100

    Fix Isseu

--- admin-panel/src/enviroment/enviroment.ts
+++ admin-panel/src/enviroment/enviroment.ts
@@ -1,9 +1,6 @@
 export const enviroment = {
-
-    production : false,
-    URL_BACKEND: 'http://127.0.0.1:8000/',
-    URL_SERVICE: 'http://127.0.0.1:8000/api',
-    URL_FRONTEND: 'http://localhost:4200',
-
-
-}
+  production: false,
+  URL_BACKEND: 'http://10.0.2.163:8080/',   // Use the private IP and the correct port
+  URL_SERVICE: 'http://10.0.2.163:8080/api', // Same here, using the private IP
+  URL_FRONTEND: 'http://localhost:4200',     // Assuming this is your frontend local setup
+};
