78bea1f7f3b3d4af3d167443ff7e5854d72b5444
NssibiOussama <oussama.ncibi@tek-up.de>
2025-02-11 18:02:47 +0100

    Fix bug

--- admin-panel/src/enviroment/enviroment.ts
+++ admin-panel/src/enviroment/enviroment.ts
@@ -1,6 +1,6 @@
 export const enviroment = {
   production: false,
-  URL_BACKEND: 'http://10.0.2.163:8080/',   // Use the private IP and the correct port
-  URL_SERVICE: 'http://10.0.2.163:8080/api', // Same here, using the private IP
-  URL_FRONTEND: 'http://localhost:4200',     // Assuming this is your frontend local setup
+  URL_BACKEND: 'http://internal-backend-alb-1888547078.us-east-1.elb.amazonaws.com:8080/',   // Use the private IP and the correct port
+  URL_SERVICE: 'http://internal-backend-alb-1888547078.us-east-1.elb.amazonaws.com:8080/api', // Same here, using the private IP
+  URL_FRONTEND: 'http://frontend-alb-920332400.us-east-1.elb.amazonaws.com:4200',     // Assuming this is your frontend local setup
 };
